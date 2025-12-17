97c20fb35c74bd02785e841ae594c6cba617a91c
codingbyjess <codingbyjess@gmail.com>
2025-12-17 19:38:07 -0300

    feat(header): create hero and add into home page

---
+++ app/containers/hero/Hero.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <section className="relative w-full h-180 md:h-(400px)">
+            <Image
+                alt="Imagem de uma placa-mãe"
+                src='/background-image.png'
+                fill
+                className="object-cover opacity-35 blur-xs"
+            />
+
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-center">
+                <h1 className="font-bold whitespace-normal xl:whitespace-nowrap text-4xl sm:text-5xl md:text-6xl text-foreground">
+                    Soluções{" "}
+                    <span className="text-(--third)">premium</span>{" "}
+                    para seu aparelho
+                </h1>
+
+                <strong className="text-foreground mt-20 max-w-xl sm:text-3xl">
+                    Micro-soldagem · Limpeza de Computadores · Recuperação de Placas de Eletrodomésticos
+                </strong>
+
+                <div className="whitespace-nowrap mt-20 space-x-4">
+                    <button className="mt-8 px-6 py-3 bg-(--third) text-foreground rounded-xl hover:bg-(--second) transition">
+                        Quero um orçamento
+                    </button>
+                    <button className="mt-8 px-6 py-3 bg-transparent text-foreground rounded-xl border-2 border-foreground transition">
+                        Falar com um consultor
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import Hero from "./containers/hero/Hero";
 
 export default function Home() {
   return (
-    <></>
+    <div>
+      <Hero />
+    </div>
   );
 }
