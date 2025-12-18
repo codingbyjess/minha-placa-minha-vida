360d47fafd4d7c8b9bdcd579e13adba15c0e372a
codingbyjess <codingbyjess@gmail.com>
2025-12-18 15:15:27 -0300

    refactor(home): add sections into home (part 2)

---
+++ app/containers/second-section/SecondSection.tsx
@@ -0,0 +1,44 @@
+import BoxService from "@/app/components/box-service/BoxService";
+
+const SecondSection = () => {
+
+    return (
+        <section className="p-10">
+            <h1 className="text-3xl text-foreground font-bold">
+                Nossos serviços
+            </h1>
+            <div className="relative justify-items-center">
+                <div className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          md:gap-10 
+          mt-5"
+                >
+                    <div>
+                        <BoxService
+                            icon="devBoard"
+                            title="Micro-soldagem"
+                        />
+                        <BoxService
+                            icon="cleaningServices"
+                            title="Limpeza de Computadores"
+                        />
+                    </div>
+                    <div className="md:mt-20">
+                        <BoxService
+                            icon="memory"
+                            title="Recuperação de Placas de Eletrodomésticos"
+                        />
+                        <BoxService
+                            icon="computer"
+                            title="Formatações"
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default SecondSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,13 @@
-import BoxService from "./components/box-service/BoxService";
 import FirstSection from "./containers/first-section/firstSection";
 import Hero from "./containers/hero/Hero";
+import SecondSection from "./containers/second-section/SecondSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <FirstSection />
-
-      <section className="p-10">
-        <h1 className="text-3xl text-foreground font-bold">
-          Nossos serviços
-        </h1>
-        <div className="relative justify-items-center">
-          <div className="
-          grid 
-          grid-cols-1 
-          md:grid-cols-2 
-          md:gap-10 
-          mt-5"
-          >
-            <div>
-              <BoxService
-                icon="devBoard"
-                title="Micro-soldagem"
-              />
-              <BoxService
-                icon="cleaningServices"
-                title="Limpeza de Computadores"
-              />
-            </div>
-            <div className="md:mt-20">
-              <BoxService
-                icon="memory"
-                title="Recuperação de Placas de Eletrodomésticos"
-              />
-              <BoxService
-                icon="computer"
-                title="Formatações"
-              />
-            </div>
-          </div>
-        </div>
-      </section>
+      <SecondSection />
     </main>
   );
 }
