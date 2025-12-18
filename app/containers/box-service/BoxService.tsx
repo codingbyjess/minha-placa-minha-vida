7f5b3ddc465c5ee34dbc1983529eac481cc63781
codingbyjess <codingbyjess@gmail.com>
2025-12-18 13:38:42 -0300

    feat(component): create boxService

--- app/containers/box-service/BoxService.tsx
+++ app/containers/box-service/BoxService.tsx
@@ -1,11 +1,60 @@
+'use client';
+import Button from '@/app/components/button/Button';
+import {
+    Memory,
+    DeveloperBoard,
+    CleaningServices,
+    Computer
+} from '@mui/icons-material';
+
+type iconName = "memory" | "devBoard" | "cleaningServices" | "computer"
+
 type BoxServiceProps = {
-    icon: string;
+    title: string,
+    icon: iconName;
 };
 
-const BoxService = () => {
+const iconMap = {
+    memory: <Memory sx={{ fontSize: 50, color: 'var(--third)' }} />,
+    devBoard: <DeveloperBoard sx={{ fontSize: 50, color: 'var(--third)' }} />,
+    cleaningServices: <CleaningServices sx={{ fontSize: 50, color: 'var(--third)' }} />,
+    computer: <Computer sx={{ fontSize: 50, color: 'var(--third)' }} />
+}
+
+const BoxService = ({ title, icon }: BoxServiceProps) => {
     return (
-        <div className="relative mt-5 w-70 h-60 border-2 border-(--second) rounded-xl flex items-center justify-center">
-            
+        <div className="
+        mb-10
+        bg-(--fourth)/30
+        w-70 
+        min-h-60 
+        border-2 
+        border-(--second) 
+        rounded-xl 
+        flex 
+        flex-col
+        p-6
+        gap-5
+        overflow-hidden
+        items-center 
+        justify-center
+        "
+        >
+
+            <div className="flex justify-center">
+                {iconMap[icon]}
+            </div>
+            <div className="flex justify-center w-full">
+                <h2 className='text-2xl text-center'>{title}</h2>
+            </div>
+            <div className="flex justify-center">
+                <Button
+                    href=''
+                    bgColor='transparent'
+                    text='Conhecer'
+                    bdColor='var(--foreground)'
+                />
+            </div>
         </div>
     );
 };
