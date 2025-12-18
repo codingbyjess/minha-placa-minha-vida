5f52bc855330a00eeafa96f0a91593b29e6974fd
codingbyjess <codingbyjess@gmail.com>
2025-12-18 13:39:46 -0300

    feat(home): create third section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
-import { Grid } from "@mui/material";
 import BoxService from "./containers/box-service/BoxService";
 import Hero from "./containers/hero/Hero";
-import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
 
 export default function Home() {
   return (
     <main>
-
       <Hero />
-
       <section className="p-10">
         <h1 className="text-3xl text-foreground font-bold">
           Seu equipamento em boas mãos
@@ -20,20 +16,46 @@ export default function Home() {
                 text-foreground"
           >
             Com um{" "}
-            <span className="text-(--third)">time especializado e tecnologia avançada</span>
+            <span className="text-(--third)">
+              time especializado e tecnologia avançada
+            </span>
             , cuidamos do seu equipamento como se fosse nosso.
           </h3>
         </div>
       </section>
 
       <section className="p-10">
-        <h1 className="text-3xl text-foreground font-bold">Nossos serviços</h1>
-        <div className="relative grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center">
-          <div className="w-180">
-            <BoxService />
-            <BoxService />
-            <BoxService />
-            <BoxService />
+        <h1 className="text-3xl text-foreground font-bold">
+          Nossos serviços
+        </h1>
+        <div className="relative justify-items-center">
+          <div className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-10 
+          mt-5"
+          >
+            <div>
+              <BoxService
+                icon="devBoard"
+                title="Micro-soldagem"
+              />
+              <BoxService
+                icon="cleaningServices"
+                title="Limpeza de Computadores"
+              />
+            </div>
+            <div className="mt-20">
+              <BoxService
+                icon="memory"
+                title="Recuperação de Placas de Eletrodomésticos"
+              />
+              <BoxService
+                icon="computer"
+                title="Formatações"
+              />
+            </div>
           </div>
         </div>
       </section>
