2f92389c47194e07dd1fc5a61443677e4b9b6d64
codingbyjess <codingbyjess@gmail.com>
2025-12-18 16:04:26 -0300

    feat(home): add third section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import FirstSection from "./containers/first-section/firstSection";
 import Hero from "./containers/hero/Hero";
 import SecondSection from "./containers/second-section/SecondSection";
+import ThirdSection from "./containers/third-section/ThirdSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <FirstSection />
       <SecondSection />
+      <ThirdSection />
     </main>
   );
 }
