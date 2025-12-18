15ff31cb11ab3edb450111fed5a66967dec6d6ee
codingbyjess <codingbyjess@gmail.com>
2025-12-18 15:12:50 -0300

    refactor(home): add sections into home

---
+++ app/containers/first-section/firstSection.tsx
@@ -0,0 +1,24 @@
+const FirstSection = () => {
+    return(
+        <section className="p-10">
+            <h1 className="text-3xl text-foreground font-bold">
+                Seu equipamento em boas mãos
+            </h1>
+            <div className="mt-4">
+                <h2 className="
+                whitespace-normal 
+                xl:whitespace-nowrap 
+                text-foreground"
+                >
+                    Com um{" "}
+                    <span className="text-(--third)">
+                        time especializado e tecnologia avançada
+                    </span>
+                    , cuidamos do seu equipamento como se fosse nosso.
+                </h2>
+            </div>
+        </section>
+    );
+}
+
+export default FirstSection;
--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
-import BoxService from "./containers/box-service/BoxService";
+import BoxService from "./components/box-service/BoxService";
+import FirstSection from "./containers/first-section/firstSection";
 import Hero from "./containers/hero/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
-      <section className="p-10">
-        <h1 className="text-3xl text-foreground font-bold">
-          Seu equipamento em boas mãos
-        </h1>
-        <div className="mt-4">
-          <h3 className="
-                whitespace-normal 
-                xl:whitespace-nowrap 
-                text-foreground"
-          >
-            Com um{" "}
-            <span className="text-(--third)">
-              time especializado e tecnologia avançada
-            </span>
-            , cuidamos do seu equipamento como se fosse nosso.
-          </h3>
-        </div>
-      </section>
+      <FirstSection />
 
       <section className="p-10">
         <h1 className="text-3xl text-foreground font-bold">
